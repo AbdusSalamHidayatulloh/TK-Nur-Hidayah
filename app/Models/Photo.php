9a89a53e67463c1e4e8abddc426ef1b8f5b86259
Counter FX <christoperjoel1@gmail.com>
2025-10-11 13:02:11 +0700

    Adding migration, seeder to database folder, inserting data to seeder and images to public

---
+++ app/Models/Photo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'title',
+        'image_path',
+    ];
+}
---
+++ database/seeders/PhotoSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Photo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PhotoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Photo::insert([
+            [
+                'title' => 'Belajar bersama native speaker',
+                'image_path' => 'image/native_speaker.jpg'
+            ],
+            [
+                'title' => 'Edukasi kesehatan bersama Yakult',
+                'image_path' => 'image/edukasi_yakult.jpg'
+            ],
+            [
+                'title' => 'Pengenalan Akademi Militer',
+                'image_path' => 'image/kirab_maulid_nabi.jpg'
+            ],
+            [
+                'title' => 'Kegiatan Kirab Maulid Nabi',
+                'image_path' => 'image/kirab_maulid_nabi.jpg'
+            ],
+            [
+                'title' => 'Manasik / Latihan Haji',
+                'image_path' => 'image/manasik_haji.jpg'
+            ],
+            [
+                'title' => 'Mengaji Ummi',
+                'image_path' => 'image/manasik_haji.jpg'
+            ],
+            [
+                'title' => 'Kegiatan belajar bahasa Jawa',
+                'image_path' => 'image/kegiatan_bahasa.jpg'
+            ]
+        ]);
+    }
+}
