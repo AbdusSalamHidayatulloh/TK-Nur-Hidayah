a5395e1c79a2ee1485cbaeaa624e9695b4f61591
Counter FX <christoperjoel1@gmail.com>
2025-10-12 11:48:56 +0700

    Header & Footer done, moving to main menu (Adding some image for hero section on main menu & social media logo on the footer)

--- resources/views/layout/default-layout.blade.php
+++ resources/views/layout/default-layout.blade.php
@@ -1,58 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>@yield("title")</title>
 </head>
-<body class="d-flex flex-column min-vh-100 overflow-hidden">
-    <header class="w-100 fixed-top d-flex flex-row justify-content-between bg-green-gradient-header justify-content-md-center  align-items-center bg-success p-3">
+
+<body class="d-flex flex-column overflow-hidden">
+    <header
+        class="w-100 fixed-top d-flex flex-row justify-content-between bg-green-gradient-header justify-content-sm-center align-items-center bg-success p-3 z-2">
         <a href="/" class="text-decoration-none d-flex flex-row h-100 align-items-center">
-            <img src="#" width="60" height="60" class="me-2"/>
-            <h4 class="text-white fw-bold mb-0 mt-0">TK Nur Hidayah</h4>
+            <img src="/image/logo.png" width="70" height="70" class="me-2" />
+            <h4 class="text-white fw-bold mb-0 mt-0">TK/KB Nur Hidayah</h4>
         </a>
-        <span class="material-symbols-rounded text-white d-md-none" style="font-size: 36px; cursor:pointer;">
+        <span class="material-symbols-rounded text-white d-sm-none" style="font-size: 36px; cursor:pointer;">
             menu
         </span>
     </header>
     <!-- For the navbar under the header -->
-    <nav class="bg-green-gradient-navbar d-flex justify-content-center py-2 gap-3 w-100">
-        <a href="/" class="text-decoration-none text-white fw-semibold">Beranda</a>
-        <a href="/programs" class="text-decoration-none text-white fw-semibold">Program</a>
-        <a href="/about-us" class="text-decoration-none text-white fw-semibold">Tentang Kami</a>
-        <a href="/enlist" class="text-decoration-none text-white fw-semibold">Pendaftaran</a>
+    <nav class="bg-green-gradient-navbar d-sm-flex justify-content-center py-2 gap-5 w-100 d-none z-2">
+        <a href="/" class="text-decoration-none text-white fw-semibold" style="font-size: 18px">Beranda</a>
+        <a href="/programs" class="text-decoration-none text-white fw-semibold" style="font-size: 18px">Program</a>
+        <a href="/about-us" class="text-decoration-none text-white fw-semibold" style="font-size: 18px">Tentang Kami</a>
+        <a href="/enlist" class="text-decoration-none text-white fw-semibold" style="font-size: 18px">Pendaftaran</a>
     </nav>
-    <main class="flex-fill">
+    <main class="z-0" style="padding-top: 100px; padding-bottom: 40px;">
         @yield('mainContent')
     </main>
-    <footer class="bg-green-gradient-footer">
-        <div class="d-flex flex-md-row flex-column py-5 justify-content-evenly align-items-center mt-auto">
-            <img width="130" height="130" src="#">
-            <div class="d-flex flex-column">
-                <a href="/" class="text-decoration-none text-white fw-semibold linksFooter" style="font-size: 18px;">
-                    <span class="material-symbols-rounded" style="vertical-align: middle; font-size: 20px;">home</span> Beranda
-                </a>
-                <hr class="mb-3 mt-3" style="border: 1px solid white; opacity: 1;">
-                <a href="/programs" class="text-decoration-none text-white fw-semibold linksFooter" style="font-size: 18px;">
-                    <span class="material-symbols-rounded" style="vertical-align: middle; font-size: 20px;">school</span> Programs
-                </a>
-                <hr class="mb-3 mt-3" style="border: 1px solid white; opacity: 1;">
-                <a href="/about-us" class="text-decoration-none text-white fw-semibold linksFooter" style="font-size: 18px;">
-                    <span class="material-symbols-rounded" style="vertical-align: middle; font-size: 20px;">emoji_people</span> Tentang Kami
-                </a>
-                <hr class="mb-3 mt-3" style="border: 1px solid white; opacity: 1;">
-                <a href="/enlist" class="text-decoration-none text-white fw-semibold linksFooter" style="font-size: 18px;">
-                    <span class="material-symbols-rounded" style="vertical-align: middle; font-size: 20px;">assignment</span> Pendaftaran
-                </a>
+    <footer class="bg-green-gradient-footer py-5 mt-auto">
+        <div class="container">
+            <div class="d-flex flex-sm-row flex-column justify-content-evenly align-items-center gap-3">
+                <img width="170" height="170" src="/image/logo.png">
+                <div class="d-flex flex-column gap-2">
+                    <a href="/" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                        style="font-size: 20px;">
+                        <span class="material-symbols-rounded"
+                            style="vertical-align: middle; font-size: 24px;">home</span> Beranda
+                    </a>
+                    <a href="/programs" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                        style="font-size: 20px;">
+                        <span class="material-symbols-rounded"
+                            style="vertical-align: middle; font-size: 24px;">school</span> Programs
+                    </a>
+                    <a href="/about-us" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                        style="font-size: 20px;">
+                        <span class="material-symbols-rounded"
+                            style="vertical-align: middle; font-size: 24px;">emoji_people</span> Tentang Kami
+                    </a>
+                    <a href="/enlist" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                        style="font-size: 20px;">
+                        <span class="material-symbols-rounded"
+                            style="vertical-align: middle; font-size: 24px;">assignment</span> Pendaftaran
+                    </a>
+                </div>
+                <!-- Sosmed -->
+                <div class="d-flex flex-column align-items-center">
+                    <p class="text-white">Kontak Kami</p>
+                    <div class="d-flex flex-row gap-3">
+                        <a href="https://www.instagram.com/kbtknurhidayah_malang">
+                            <img src="image/instagram.png" width="50" height="50"
+                                class="rounded border border-2 border-white" alt="Instagram Logo" />
+                        </a>
+                        <a href="https://api.whatsapp.com/send?phone=6285706339931&text=Halo%20saya%20ingin%20bertanya%20tentang%20TK%20KB%20Nur%20Hidayah" target="_blank" rel="noopener noreferrer">
+                            <img src="image/whatsapp.png" width="50" height="50"
+                                class="rounded border border-2 border-white" alt="Whatsapp Logo"/>
+                        </a>
+                        <a href="mailto:tknurhidayah57a@gmail.com">
+                            <img src="image/gmail.png" width="50" height="50"
+                                class="rounded border border-2 border-white" alt="Gmail Logo"/>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-HoA13XY0ZyaFqPh2V6F4fy1R6n+YtFTo8ML1v+CTe3YHjpyMfdqUbn7Q3Z3DoaTo" crossorigin="anonymous">
-</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HoA13XY0ZyaFqPh2V6F4fy1R6n+YtFTo8ML1v+CTe3YHjpyMfdqUbn7Q3Z3DoaTo" crossorigin="anonymous">
+        </script>
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,14 @@
 @extends("layout.default-layout")
 @section("title", "Selamat Datang!")
+@section("mainContent")
+    <div class="hero d-flex justify-content-center align-items-center text-center position-relative">
+        <img src="/image/senam_bersama.jpg" class="w-100 object-fit-cover" style="height: 700px; object-position: center; filter: brightness(50%);">
+        <div class="position-absolute top-50 start-50 translate-middle text-white text-center">
+            <h3>Selamat Datang Di</h3>
+            <h1 class="fw-bolder">TK/KB Nur Hidayah!</h1>
+            <a href="/about-us" class="btn btn-primary">
+                Yuk Pelajari Kami! <span class=""></span>
+            </a>
+        </div>
+    </div>
+@endsection
