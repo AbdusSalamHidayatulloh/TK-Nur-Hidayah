1315976439468a14f72ebb49b4883d3869f10ef8
chris <christoperjoel1@gmail.com>
2025-11-04 21:12:39 +0700

    modified teacher's table migration

--- database/migrations/2025_10_17_120826_create_teachers_table.php
+++ database/migrations/2025_10_17_120826_create_teachers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('position');
             $table->string('image')->nullable();
             $table->integer('age');
