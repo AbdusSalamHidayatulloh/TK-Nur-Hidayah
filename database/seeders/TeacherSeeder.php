66de8a28ba2261c4ca4b27b729b23fed58a7d69e
chris <christoperjoel1@gmail.com>
2025-11-04 21:13:08 +0700

    Modify Teacher Seeder for login purpose

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,20 @@ class TeacherSeeder extends Seeder
      */
     public function run(): void
     {
-        //TODO: WIll add seeders here
+        $annaUser = User::where('email', 'anna@nur.hidayah.com')->first();
+        Teacher::create([
+            'user_id' => $annaUser->id,
+            'position' => 'Kindergarten Teacher',
+            'age' => 28,
+            'image' => '/image/guru1'
+        ]);
+
+        $muhammadUser = User::where('email', 'Muhammad@nur.hidayah.com')->first();
+        Teacher::create([
+            'user_id' => $muhammadUser->id,
+            'position' => 'Preschool Teacher',
+            'age' => 32,
+            'image' => '/image/guru2'
+        ]);
     }
 }
