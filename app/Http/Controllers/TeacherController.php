9d3ce1e7d9c11cb549ee3a4c8d6e2fc89e9a3bfb
chris <christoperjoel1@gmail.com>
2025-11-05 11:34:21 +0700

    Adding create, update, delete & changes to the read

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TeacherRequest;
 use App\Models\Teacher;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class TeacherController extends Controller
@@ -15,16 +18,16 @@ class TeacherController extends Controller
     public function indexTeacher(Request $request) {
         if($request->has('teacherSearch')) {
                 return view('teacher', [ 
-                'sitename' => $request->searchStudent,
-                'maintitle' => 'Searched Murid: '.$request->searchStudent,
-                'teachers' => Teacher::where('name', 'like', '%'.$request->searchStudent.'%')
+                'sitename' => $request->teacherSearch,
+                'maintitle' => 'Searched Murid: '.$request->teacherSearch,
+                'teachers' => Teacher::where('name', 'like', '%'.$request->teacherSearch.'%')
                 ->paginate(10)
                 ->withQueryString(),
             ]);
         } else {
             return view('teacher', [
-                'sitename' => $request->searchStudent,
-                'maintitle' => 'Searched Murid: '.$request->searchStudent,
+                'sitename' => $request->teacherSearch,
+                'maintitle' => 'Searched Murid: '.$request->teacherSearch,
                 'teachers' => Teacher::paginate(10)
             ]);
         }
@@ -35,7 +38,82 @@ class TeacherController extends Controller
             'sitename' => $teacher->user->name,
             'maintitle' => $teacher->user->name."'s Data",
             'teacher' => $teacher,
-            'classroom' => $teacher->classrooms->name
+            'classroom' => $teacher->classrooms?->name
         ]);
+    }
+
+    public function addTeacher(TeacherRequest $request) {
+        $user = $request->user();
+        if($user->role === 'admin') {
+            $validateData = $request->validate($request->rulesForCreate());
+
+            $newUser = User::create([
+                'name' => $validateData['name'],
+                'email' => $validateData['email'],
+                'password' => bcrypt($validateData['password']),
+                'role' => 'teacher'
+            ]);
+
+            Teacher::create([
+                'user_id' => $newUser->id,
+                'position' => $validateData['position'],
+                'birthdate' => $validateData['birthdate']
+            ]);
+        }
+    }
+
+    public function deleteTeacher(TeacherRequest $request, int $userId): RedirectResponse {
+        //Find the teacher
+        $teacherUser = User::findOrFail($userId);
+
+        if($teacherUser->role !== 'teacher') {
+            abort(400, 'Cannot delete a non teacher user (admin)');
+        }
+
+        $teacherUser->delete();
+
+        return back()->with('Success', 'Teacher has been deleted');
+    } 
+
+    public function updateTeacher(TeacherRequest $request, int $userId) {
+        $currentUser = $request->user();
+        if($currentUser->role === 'admin') {
+            $validateData = $request->validate($request->rulesForUpdate($userId));
+            $user = User::findOrFail($userId);
+            $teacher = $user->teacher;
+
+            $user->update([
+                'name' => $validateData['name'] ?? $user->name, 
+                'email' => $validateData['email'] ?? $user->email,
+                'role' => $validateData['role'] ?? $user->role
+            ]);
+
+            $teacher->update([
+                'position' => $validateData['position'] ?? $teacher->position,
+                'image' => $validateData['image'] ?? $teacher->image,
+                'birthdate' => $validateData['birthdate'] ?? $teacher->birthdate
+            ]);
+        } elseif ($currentUser->role === 'teacher') {
+            $validateData = $request->validated($request->rulesForUpdate($userId));
+            $user = User::findOrFail($userId);
+            $teacher = $user->teacher;
+
+            $user->update([
+                'name' => $validateData['name'] ?? $user->name, 
+                'email' => $validateData['email'] ?? $user->email,
+                'password' => 
+                //Check if there's a changes in the password field
+                    !empty($validateData['password'])
+                    ? bcrypt($validateData['password'])
+                    : $user->password,
+                'role' => $validateData['role'] ?? $user->role
+            ]);
+
+            $teacher->update([
+                'position' => $validateData['position'] ?? $teacher->position,
+                'image' => $validateData['image'] ?? $teacher->image,
+                'birthdate' => $validateData['birthdate'] ?? $teacher->birthdate
+            ]);
+        }
     }
 }
