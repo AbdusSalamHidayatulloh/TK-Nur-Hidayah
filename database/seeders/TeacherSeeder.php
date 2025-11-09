127f12e49acae9f7c3f84a8b0f6d5829a98e95d4
chris <christoperjoel1@gmail.com>
2025-11-09 20:39:16 +0700

    File path change for the image

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -19,7 +19,7 @@ class TeacherSeeder extends Seeder
             'user_id' => $annaUser->id,
             'position' => 'Guru Kelas A',
             'birthdate' => "29/11/2000",
-            'image' => '/image/guru1.jpg'
+            'image' => '/image/teacherImage/guru1.jpg'
         ]);
 
         $muhammadUser = User::where('email', 'Muhammad@nur.hidayah.com')->first();
@@ -27,14 +27,14 @@ class TeacherSeeder extends Seeder
             'user_id' => $muhammadUser->id,
             'position' => 'Guru Kelas B',
             'birthdate' => "12/03/1998",
-            'image' => '/image/guru2.jpg'
+            'image' => '/image/teacherImage/guru2.jpg'
         ]);
         $joelUser = User::where('email', 'joel@nur.hidayah.com')->first();
         Teacher::create([
             'user_id' => $joelUser->id,
             'position' => 'Guru Kelas 2C',
             'birthdate' => "29/05/2001",
-            'image' => '/image/guru3.jpg',
+            'image' => '/image/teacherImage/guru3.jpg',
         ]);
     }
 }
