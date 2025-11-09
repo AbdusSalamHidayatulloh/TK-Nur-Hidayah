528fb22dc341a05c9456e7e1fd09d14679bf16dc
chris <christoperjoel1@gmail.com>
2025-11-09 19:51:19 +0700

    Changes to the program to connect using foreign key to Events (new migration)

---
+++ database/migrations/2025_11_09_124018_events.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *  'event_name',
+        'event_date_start',
+        'event_date_end',
+        'event_description'
+     */
+    public function up(): void
+    {
+        Schema::create('events', function(Blueprint $table) {
+            $table->id();
+            $table->string('event_name');
+            $table->string('event_date_start');
+            $table->string('event_date_end');
+            $table->string('event_description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
