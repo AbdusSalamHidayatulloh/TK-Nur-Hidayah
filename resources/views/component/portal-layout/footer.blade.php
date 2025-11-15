c22fca27c023588ac616b10ae685c5d12c278cbe
chris <christoperjoel1@gmail.com>
2025-11-15 16:24:53 +0700

    Modularization of static layout & portal layout (new)

---
+++ resources/views/component/portal-layout/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="bg-green-gradient-footer py-5 mt-auto">
+    <div class="container">
+        <div class="d-flex flex-md-row flex-column justify-content-evenly align-items-center gap-4">
+            <img width="170" height="170" src="/image/logo/logo.png">
+            <div class="d-flex flex-column gap-2">
+                <a href="/student-list" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                    style="font-size: 20px;">
+                    <span class="material-symbols-rounded"
+                        style="vertical-align: middle; font-size: 24px;">school</span> List Murid
+                </a>
+                <a href="/teacher-list" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                    style="font-size: 20px;">
+                    <span class="material-symbols-rounded"
+                        style="vertical-align: middle; font-size: 24px;">emoji_people</span> List Guru
+                </a>
+                <a href="/event-list" class="text-decoration-none text-white fw-semibold linksFooter mb-0 mt-0"
+                    style="font-size: 20px;">
+                    <span class="material-symbols-rounded"
+                        style="vertical-align: middle; font-size: 24px;">assignment</span> Event
+                </a>
+            </div>
+            <!-- Somded -->
+            <div class="d-flex flex-column align-items-center">
+                <p class="text-white">Jika ada masalah, hubungi operator</p>
+                <div class="d-flex flex-row gap-3">
+                    <a href="https://api.whatsapp.com/send?phone=6285706339931&"
+                        target="_blank" rel="noopener noreferrer">
+                        <img src="/image/logo/whatsapp.png" width="50" height="50"
+                            class="rounded border border-2 border-white" alt="Whatsapp Logo"/>
+                    </a>
+                </div>
+                <p class="pt-4 text-white">©2025 Nur Hidayah</p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/component/portal-layout/mobile-menu.blade.php
@@ -0,0 +1,14 @@
+<div id="mobile-menu"
+    class="mobile-sidebar d-md-none position-fixed end-0 top-0 d-flex p-1 flex-column text-white gap-4 rounded rounded-start-5 shadow-sm gap-3 z-5">
+    <div class="w-100 d-flex justify-content-center align-items-center">
+        <img src="/image/logo/logo.png" width="120" height="120" alt="TK/KB Nur Hidayah logo">
+    </div>
+    <div class="d-flex justify-content-center">
+        <hr class="mt-0 mb-0 border border-1 border-black w-75">
+    </div>
+    <a href="/student-list" class="text-decoration-none w-100 text-black fs-5 fw-bolder mx-4">List Murid</a>
+    <a href="/teacher-list" class="text-decoration-none w-100 text-black fs-5 fw-bolder mx-4">List Guru</a>
+    <a href="/event-list" class="text-decoration-none w-100 text-black fs-5 fw-bolder mx-4">Event</a>
+    <a href="/" class="text-decoration-none w-100 text-black fs-5 fw-bolder mx-4">Logout</a>
+</div>
+<div id="overlay" class="overlay position-fixed top-0 start-0 w-100 h-100 d-md-none z-3"></div>
